1917a97bb3c385d9eba3a4f0767862e7315fa768
wei chen <weichen9083@gmail.com>
2020-12-01 23:09:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,39 @@
 class Song 
+  
+  attr_accessor :name, :artist, :genre
+  @@count  = 0 
+  @@genres = []
+  @@artists = []
+  
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre
+    @@artists << artist
+    @@count +=1 
+  end 
+  
+  def self.count 
+    @@count
+  end 
+  
+  def self.artists 
+    @@artists.uniq
+  end 
+  
+  def self.genres 
+    @@genres.uniq
+  end 
+  
+  def self.genre_count 
+    count_hash = {}
+    @@genres.each do |genre|
+      if genre
+    end 
+    
+  end 
+  
+  def self.artist_count 
+  end 
 end 
