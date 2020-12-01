2fd6f07d5d38f1b5210e3f7e4d755dca99af1b43
wei chen <weichen9083@gmail.com>
2020-12-01 23:17:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,8 @@ class Song
     count_hash = {}
     @@genres.each do |genre|
       if count_hash.key?(genre)
-        
+        x = count_hash[genre] +1 
+        count_hash[genre] = x 
       else 
         count_hash[genre] = 1 
       end 
